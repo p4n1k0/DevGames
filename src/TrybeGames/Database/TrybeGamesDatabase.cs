f4dfd4c1f48d746ba76b7213ff29a04689eab2cb
p4n1k0 <gabre.tarick@gmail.com>
2025-02-20 22:12:27 -0300

    funcionalidade de buscar tds os diferentes tipos de jogos

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -43,8 +43,7 @@ public class TrybeGamesDatabase
     // 8. Crie a funcionalidade de buscar todos os diferentes Tipos de jogos dentre os jogos cadastrados
     public List<GameType> GetGameTypes()
     {
-        // Implementar
-        throw new NotImplementedException();
+        return Games.Select(game => game.GameType).Distinct().ToList();
     }
 
     // 9. Crie a funcionalidade de buscar todos os estúdios de jogos junto dos seus jogos desenvolvidos com suas pessoas jogadoras
